de7245d3eb40f326e7baa99ddf37d63e4bc1077b
Rahul <rahulpratik1212@gmail.com>
2024-10-12 08:46:31 +0530

    comment color array

--- planet/src/main.js
+++ planet/src/main.js
@@ -50,7 +50,7 @@ scene.add(backgroundSphere);
 
 const radius = 1;
 const segments = 65;
-const colors = [0x00ff00, 0x0000ff, 0xff0000, 0xffff00];
+// const colors = [0x00ff00, 0x0000ff, 0xff0000, 0xffff00];
 const textureImages = ['./resources/csilla/color.png', './resources/earth/map.jpg', './resources/venus/map.jpg', './resources/volcanic/color.png'];
 const orbitRadius = 3.5;
 const spheres = new THREE.Group();
